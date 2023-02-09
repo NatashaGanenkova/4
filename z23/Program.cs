e4c87a086a49a8b4a65d6b574b974f7e26a297de
Nata <ganenkova@mail.ru>
2023-02-09 15:33:18 +0300

    Задание23

---
+++ z23/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+if (N < 0)
+{
+    N =N *(-1); 
+    }
+for (int j=1;j<= N; j++)
+{   
+     Console.WriteLine($"{Math.Pow(j,3)}");
+
+}
+
