3d7ee0e71203a5293fd10ac422650afc533c9aaa
Nata <ganenkova@mail.ru>
2023-02-09 15:23:37 +0300

    Задание19

---
+++ z19/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+int i = num / 10000;
+int b = num % 10;
+int c = num / 1000;
+int d = c % 10;
+int f = num % 100;
+int g = f/10;
+
+if (i == b && d==g)
+{
+    Console.WriteLine("Является палиндромом");
+    return;
+}
+
+else
+{
+    Console.WriteLine("Не является палиндромом");
+}
